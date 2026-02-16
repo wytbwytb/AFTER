21347343a09cffa2def4b739bf5fdf7d5609c210
wytbwytb <56542297+wytbwytb@users.noreply.github.com>
2026-02-16 12:44:29 +0800

    Update train_estimator.sh

--- scripts/train_estimator.sh
+++ scripts/train_estimator.sh
@@ -1,7 +1,3 @@
-# python train_estimator.py --model llava_v1.5_7B_lht \
-#     --query_set POPE_train_I+Q --caption_set POPE_train_T+Q_query --vector_set POPE_train_T+Q_best --epochs 10 \
-#     --save_path /path/to/your/workdir/after/probes/llava_v1.5_7B_lht_offset_generator_YR
-
 python train_estimator.py --model llava_v1.5_7B_lht \
-    --query_set POPE_train_YR_I+Q --caption_set POPE_train_YR_C_p2+Q_query --vector_set POPE_train_YR_C_p2+Q_best --epochs 10 \
-    --save_path /path/to/your/workdir/AFTER/probes/llava_v1.5_7B_lht_offset_generator_YR
+    --query_set POPE_train_I+Q --caption_set POPE_train_T+Q_query --vector_set POPE_train_T+Q_best --epochs 10 \
+    --save_path /path/to/your/workdir/after/probes/llava_v1.5_7B_lht_offset_generator_YR
